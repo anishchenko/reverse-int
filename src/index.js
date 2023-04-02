3128c49e7bf93afdc6ed179a522f39547b1cd75f
oleksandr anishchenko <anishchenko.alexandr@gmail.com>
2023-04-02 13:19:40 +0300

    feat: all tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const numStr = String(Math.abs(n));
+  let num = '';
+
+  for(let i = numStr.length - 1; i >= 0; i--) {
+    num += numStr[i];
+  }
+  return num;
 }
